7d035f3f1ddcc664e67847d048cf75593fcf2f98
tdenolle <thomas.denolle@gmail.com>
2024-07-01 20:59:02 +0200

    Json files move to base app folder

--- Docker.entrypoint.sh
+++ Docker.entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 cd /
-python -m app.PoolAccessMqttBridge --config=/data/options.json --sensors=/data/sensors.json
+python -m app.PoolAccessMqttBridge --config=/data/options.json --sensors=/app/sensors.json
