f78e55a8704ffe99ee4a4c569220ab452bded192
tdenolle <thomas.denolle@gmail.com>
2024-12-20 13:47:09 +0100

    Update Entity platform setup

--- app/hass/Update.py
+++ app/hass/Update.py
@@ -4,10 +4,12 @@ from app.hass.Entity import Entity
 
 
 class Update(Entity):
+    ENTITY_PLATFORM = "update"
+
     def __init__(self, data: dict, device: BayrolPoolaccessDevice, dicovery_prefix: str = "homeassistant"):
         super().__init__(data, device, dicovery_prefix)
+        self._attributes["platform"] = self.ENTITY_PLATFORM
 
     @property
     def type(self) -> str:
-        return "update"
-
+        return self.ENTITY_PLATFORM
