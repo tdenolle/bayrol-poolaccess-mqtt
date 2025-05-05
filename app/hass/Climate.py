6637d4688cb1ae43815a3dce2630c2227e09a790
Loïc <1536036+zoic21@users.noreply.github.com>
2025-05-05 17:16:09 +0200

    test

--- app/hass/Climate.py
+++ app/hass/Climate.py
@@ -6,15 +6,17 @@ from app.hass.Entity import Entity
 class Climate(Entity):
     def __init__(self, data: dict, device: BayrolPoolaccessDevice, dicovery_prefix: str = "homeassistant"):
         super().__init__(data, device, dicovery_prefix)
-        
 
+        if "config_topic" not in data:
+            data["config_topic"] = "%s/config" % data["state_topic"]
+        
     @property
     def type(self) -> str:
         return "climate"
 
     @property
-    def command_topic(self) -> str:
-        return self._attributes["command_topic"]
+    def config_topic(self) -> str:
+        return self._attributes["config_topic"]
 
     def build_config(self):
         return super().build_config()
