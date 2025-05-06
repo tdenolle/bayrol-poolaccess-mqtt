9c39f83be98af7a3f2fceb1ff45b6d2bbf4684be
Loïc <1536036+zoic21@users.noreply.github.com>
2025-05-06 10:18:20 +0200

    test

--- app/hass/Entity.py
+++ app/hass/Entity.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 from .BayrolPoolaccessDevice import BayrolPoolaccessDevice
 from ..Translation import LanguageManager
+from Switch import Switch
+from Climate import Climate
 
 
 def norm(s: str):
