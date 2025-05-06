ea816423de5475d5cff0e5e0527df14cf9c2b98a
Loïc <1536036+zoic21@users.noreply.github.com>
2025-05-06 11:48:43 +0200

    add pump mode

--- app/PoolAccessMqttBridge.py
+++ app/PoolAccessMqttBridge.py
@@ -27,6 +27,7 @@ from paho.mqtt.client import MQTTMessage, MQTT_ERR_SUCCESS
 
 from app.Translation import LanguageManager
 from .hass.Switch import Switch
+from .hass.Select import Select
 from .hass.Climate import Climate
 from .hass.BayrolPoolaccessDevice import BayrolPoolaccessDevice
 from .hass.Entity import Entity
@@ -125,7 +126,7 @@ class PoolAccessMqttBridge:
             self._logger.info("[mqtt] connect: [%s][%s][%s]", str(rc), str(userdata), str(flags))
             # Looping on entities
             for e in self._hass_entities:  # type: Entity
-                if isinstance(e, Switch):
+                if isinstance(e, Switch) or isinstance(e, Select):
                     # Subscribing to Entity Messages
                     self._logger.info("Subscribing to topic: %s", e.command_topic)
                     self._brocker_client.subscribe(e.command_topic)
